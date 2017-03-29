39a409ad20811e0ad5c0b455cbfa2a9b91daff7f
Joseph Porter <joporter5606@live.com>
2017-03-28 22:42:23 -0700

    Think the encrypt and decrypt part of AES should work.  The key is not being set since on the command line we are passing the key as bytes so like in des we have to convert two characters into its byte format

--- AES.cpp
+++ AES.cpp
@@ -10,37 +10,31 @@
  */
 bool AES::setKey(const unsigned char* keyArray)
 {
-
-	// TODO: AES implementation of openssl cares about whether
-	// you are encrypting or decrypting when setting the key.
-	// That is, when encrypting you use function AES_set_encrypt_key(...)
-	// and when decrypting AES_set_decrypt_key(...).
-	//
-	// One way to solve this problem is to pass in a 17 byte key, where
-	// the first byte is used to indicate whether we are encrypting or
-	// decrypting. E.g., if the first byte is 0, then use AES_set_encrypt_key(...).
-	// Otherwise, use AES_set_decrypt_key(...).  The rest of the bytes in the
-	// array indicate the 16 bytes of the 128-bit AES key.
-	//
-	// Both functions return 0 on success and other values on faliure.
-	// For documentation, please see https://boringssl.googlesource.com/boringssl/+/2623/include/openssl/aes.h
-	// and aes.cpp example provided with the assignment.
-
 	// Check if the array passed is 17 bytes long
-	if(strlen((char *) keyArray) == 17){
+	if(strlen((char *) keyArray) == 18){
 		if(keyArray[0] == '0'){
 			// The first byte was a 0, use AES_set_encrypt_key(...)
-			AES_set_encrypt_key(keyArray+1, 128, &key);
-			return true;
+			if (AES_set_encrypt_key(keyArray+1, 128, &key) != 0)
+			{
+				fprintf(stderr, "AES_set_encrypt_key failed!\n");
+				return false;
+			}
 		}
 		else{
 			// The first byte was not 0, use AES_set_decrypt_key(...)
-			AES_set_decrypt_key(keyArray+1, 128, &key);
-			return true;
+			if (AES_set_decrypt_key(keyArray+1, 128, &key) != 0)
+			{
+				fprintf(stderr, "AES_set_decrypt_key failed!\n");
+				return false;
+			}
 		}
 	}
-	return false;
-	
+	else
+	{
+		return false;
+	}
+	return true;
+
 }
 
 /**
@@ -50,13 +44,12 @@ bool AES::setKey(const unsigned char* keyArray)
  */
 unsigned char* AES::encrypt(const unsigned char* plainText)
 {
+	// Size of block we don't care about null terminated since block will be padded and will always be 16 bytes
+	unsigned char encrypted_output[16];
 
-	//TODO: 1. Dynamically allocate a block to store the ciphertext.
-	//	2. Use AES_ecb_encrypt(...) to encrypt the text (please see the URL in setKey(...)
-	//	and the aes.cpp example provided.
-	// 	3. Return the pointer to the ciphertext
+	AES_ecb_encrypt(plainText, encrypted_output, &key, AES_ENCRYPT);
 
-	return NULL;
+	return encrypted_output;
 }
 
 /**
@@ -66,11 +59,9 @@ unsigned char* AES::encrypt(const unsigned char* plainText)
  */
 unsigned char* AES::decrypt(const unsigned char* cipherText)
 {
+	unsigned char decrypted_output[16];
 
-	//TODO: 1. Dynamically allocate a block to store the plaintext.
-	//	2. Use AES_ecb_encrypt(...) to decrypt the text (please see the URL in setKey(...)
-	//	and the aes.cpp example provided.
-	// 	3. Return the pointer to the plaintext
+	AES_ecb_encrypt(cipherText, decrypted_output, &key, AES_DECRYPT);
 
-	return NULL;
+	return decrypted_output;
 }
